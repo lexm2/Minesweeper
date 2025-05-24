519c50636ae0fa53cfe7609924970a7bf552a6e4
lexm2 <lex.k.moulton@gmail.com>
2025-05-24 01:56:09 -0400

    Create minesweeper.js

---
+++ minesweeper.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Minesweeper Hacks
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Userscript for Minesweeper
+// @author       Big Chungus
+// @match        https://minesweeperonline.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=minesweeperonline.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Your minesweeper code here
+    console.log('Minesweeper userscript loaded!');
+    
+    // If you want to import your external minesweeper.js file
+    function loadExternalScript() {
+        const script = document.createElement('script');
+        script.src = 'file:///path/to/your/minesweeper.js'; // This won't work directly due to security restrictions
+        document.head.appendChild(script);
+    }
+    
+    // Alternative: Copy your minesweeper.js code here
+})();
